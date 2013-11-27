bfc19aa33fc9b3d13015371d068420de4a31e90c
Bert De Geyter <bertieboy090@hotmail.com>
2013-11-27 17:46:33 +0100

    small fix

--- src/com/github/theholywaffle/teamspeak3/example/Example1.java
+++ src/com/github/theholywaffle/teamspeak3/example/Example1.java
@@ -11,55 +11,55 @@ import com.github.theholywaffle.teamspeak3.api.wrapper.Channel;
 
 public class Example1 {
 
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 
-		TS3Api api = new TS3Query("77.77.77.77", TS3Query.DEFAULT_PORT,
-				FloodRate.DEFAULT).debug().connect().getBot();
+                TS3Api api = new TS3Query("77.77.77.77", TS3Query.DEFAULT_PORT,
+                                FloodRate.DEFAULT).debug().connect().getBot();
 
-		api.login("serveradmin", "serveradminpassword");
-		api.selectDefaultVirtualServer();
-		api.setNickname("PutPutBot");
-		api.addTS3Listeners(new DefaultListener());
+                api.login("serveradmin", "serveradminpassword");
+                api.selectDefaultVirtualServer();
+                api.setNickname("PutPutBot");
+                api.addTS3Listeners(new DefaultListener());
 
-		api.registerAllEvents();
-	}
+                api.registerAllEvents();
+        }
 
-	public static void troll(String name, TS3Api bot) {
-		ArrayList<Channel> channels = new ArrayList<>();
-		for (Channel c : bot.getChannels()) {
-			if (c.getTotalClients() > 0) {
-				channels.add(c);
-			}
-		}
+        public static void troll(String name, TS3Api bot) {
+                ArrayList<Channel> channels = new ArrayList<>();
+                for (Channel c : bot.getChannels()) {
+                        if (c.getTotalClients() > 0) {
+                                channels.add(c);
+                        }
+                }
 
-		int clientId = bot.getClientByName(name).get(0).getId();
+                int clientId = bot.getClientByName(name).get(0).getId();
 
-		Random r = new Random();
-		while (true) {
-			int channelId = channels.get(r.nextInt(channels.size())).getId();
-			bot.moveClient(clientId, channelId);
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                Random r = new Random();
+                while (true) {
+                        int channelId = channels.get(r.nextInt(channels.size())).getId();
+                        bot.moveClient(clientId, channelId);
+                        try {
+                                Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                                e.printStackTrace();
+                        }
+                }
+        }
 
-	public static void troll2(String name, TS3Api bot) {
-		int clientId = bot.getClientByName(name).get(0).getId();
+        public static void troll2(String name, TS3Api bot) {
+                int clientId = bot.getClientByName(name).get(0).getId();
 
-		int i = 0;
-		Random r = new Random();
-		while (true) {
-			try {
-				Thread.sleep(r.nextInt(30_000) + 30_000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			bot.pokeClient(clientId, i + "");
-			i++;
-		}
-	}
+                int i = 0;
+                Random r = new Random();
+                while (true) {
+                        try {
+                                Thread.sleep(r.nextInt(30_000) + 30_000);
+                        } catch (InterruptedException e) {
+                                e.printStackTrace();
+                        }
+                        bot.pokeClient(clientId, i + "");
+                        i++;
+                }
+        }
 
 }
