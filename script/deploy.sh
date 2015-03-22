bcff0339f9696faa63c8a9a17ab06bf64ed9982c
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2015-03-22 23:15:21 +0100

    Changes to pom.xml, .travis.yml and new buildscripts so we can use Travis as a CI/CD server.

---
+++ script/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "${TRAVIS_PULL_REQUEST}" == "true" ] || [ "${TRAVIS_BRANCH}" != "master" ]; then
+	echo "Not master branch or pull request, aborting deployment"
+	exit 0
+fi
+
+version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+echo "Project version: $version"
+
+echo "Writing settings.xml"
+echo "<settings><servers><server><id>github</id><password>${OAUTH2_DEPLOY_TOKEN}</password></server></servers></settings>" > ~/settings.xml
+
+if [[ "$version" == *SNAPSHOT ]]; then
+	echo "Snapshot build, deploying artifacts"
+	echo "Running 'mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V'"
+	mvn deploy --settings ~/settings.xml -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+else
+	echo "Release build, deploying artifacts and publishing javadocs"
+	echo "Running 'mvn install -DskipTests=true -B -V'"
+	mvn deploy --settings ~/settings.xml -DskipTests=true -B -V
+fi
