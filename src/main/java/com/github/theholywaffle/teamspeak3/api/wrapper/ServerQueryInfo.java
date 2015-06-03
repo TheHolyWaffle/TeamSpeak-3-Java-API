76156a435257cb0f0587a29cfe9b3cddd9b1c452
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2015-06-03 13:39:19 +0200

    Add Javadocs to ServerQueryInfo, deprecated getId() -> getClientId().

--- src/main/java/com/github/theholywaffle/teamspeak3/api/wrapper/ServerQueryInfo.java
+++ src/main/java/com/github/theholywaffle/teamspeak3/api/wrapper/ServerQueryInfo.java
@@ -26,54 +26,167 @@ package com.github.theholywaffle.teamspeak3.api.wrapper;
  * #L%
  */
 
-import java.util.Map;
-
+import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.TS3ApiAsync;
 import com.github.theholywaffle.teamspeak3.api.ClientProperty;
 import com.github.theholywaffle.teamspeak3.api.VirtualServerProperty;
 import com.github.theholywaffle.teamspeak3.api.VirtualServerStatus;
 
+import java.util.Map;
+
+/**
+ * A class containing information about a server query, returned by the
+ * API methods {@link TS3Api#whoAmI()} and {@link TS3ApiAsync#whoAmI()}.
+ */
 public class ServerQueryInfo extends Wrapper {
 
+	/**
+	 * Creates a new {@code ServerQueryInfo} from the information present in the provided map.
+	 *
+	 * @param map
+	 * 		the map containing the key-value pairs abstracted by this object
+	 */
 	public ServerQueryInfo(Map<String, String> map) {
 		super(map);
 	}
 
+	/**
+	 * Gets the ID of the channel the server query is currently in.
+	 *
+	 * @return the ID of the current channel
+	 */
 	public int getChannelId() {
 		return getInt("client_channel_id");
 	}
 
+	/**
+	 * Gets the ID of the server query in the database.
+	 * <p>
+	 * In case this server query account was created by a client,
+	 * its database ID will be identical to the client's database ID.
+	 * </p>
+	 *
+	 * @return the server query's database ID
+	 */
 	public int getDatabaseId() {
 		return getInt(ClientProperty.CLIENT_DATABASE_ID);
 	}
 
-	public int getId() {
+	/**
+	 * Gets the client ID of the server query.
+	 *
+	 * @return the server query's client ID
+	 */
+	public int getClientId() {
 		return getInt("client_id");
 	}
 
+	/**
+	 * Gets the client ID of the server query.
+	 *
+	 * @return the server query's client ID
+	 *
+	 * @deprecated {@code getId()} is too unspecific and doesn't fit in the current naming scheme.
+	 * Use {@link #getClientId()} instead.
+	 */
+	@Deprecated
+	public int getId() {
+		return getClientId();
+	}
+
+	/**
+	 * Gets the username that was used when logging the query in (using a username-password combination).
+	 * <p>
+	 * This username was set when creating the server query login and doesn't have to be related to
+	 * the client who created the server query login.
+	 * <br>
+	 * In case a server query is not logged in yet, this method will return an empty string.
+	 * </p>
+	 *
+	 * @return the username used when logging this query in
+	 *
+	 * @see TS3Api#login(String, String) TS3Api#login(username, password) - logging in server queries
+	 */
 	public String getLoginName() {
 		return get(ClientProperty.CLIENT_LOGIN_NAME);
 	}
 
+	/**
+	 * Gets the nickname currently used by the server query.
+	 * Unless explicitly set, the nickname will be formatted like
+	 * <pre><i>username</i> from <i>ip</i>:<i>port</i></pre>
+	 * <p>
+	 * Nicknames are only assigned after a virtual server has been selected.
+	 * Until then, this method will return an empty string.
+	 * </p>
+	 *
+	 * @return the current nickname of the server query
+	 */
 	public String getNickname() {
 		return get(ClientProperty.CLIENT_NICKNAME);
 	}
 
+	/**
+	 * Gets the ID of the virtual server on which the server query login was created.
+	 * <p>
+	 * This method will return {@code 0} (the ID of the template server) if a server query is
+	 * not logged in or using the {@code serveradmin} login.
+	 * </p>
+	 *
+	 * @return the ID of the virtual server this server query belongs to
+	 */
 	public int getOriginServerId() {
 		return getInt("client_origin_server_id");
 	}
 
+	/**
+	 * Gets the unique identifier of the server query.
+	 * <p>
+	 * In case this server query account was created by a client,
+	 * its unique ID will be identical to the client's unique ID.
+	 * </p>
+	 *
+	 * @return the server query's unique ID
+	 */
 	public String getUniqueIdentifier() {
 		return get(ClientProperty.CLIENT_UNIQUE_IDENTIFIER);
 	}
 
+	/**
+	 * Gets the ID of the currently selected virtual server.
+	 * <p>
+	 * If used on a non-commercial TeamSpeak instance which can only host 1 virtual server,
+	 * this ID will always be 1.
+	 * <br>
+	 * If no virtual server has been selected yet, this method will return {@code 0}.
+	 * </p>
+	 *
+	 * @return the ID of the current virtual server or {@code 0} if none is selected
+	 */
 	public int getVirtualServerId() {
 		return getInt(VirtualServerProperty.VIRTUALSERVER_ID);
 	}
 
+	/**
+	 * Gets the port used by the currently selected virtual server.
+	 * <p>
+	 * If no virtual server has been selected yet, this method will return {@code 0}.
+	 * </p>
+	 *
+	 * @return the port used by the current virtual server or {@code 0} if none is selected
+	 */
 	public int getVirtualServerPort() {
 		return getInt(VirtualServerProperty.VIRTUALSERVER_PORT);
 	}
 
+	/**
+	 * Gets the status of the currently selected virtual server.
+	 * <p>
+	 * If no virtual server has been selected yet, this method will return {@link VirtualServerStatus#UNKNOWN}.
+	 * </p>
+	 *
+	 * @return the status of the current virtual server
+	 */
 	public VirtualServerStatus getVirtualServerStatus() {
 		final String status = get(VirtualServerProperty.VIRTUALSERVER_STATUS);
 		for (final VirtualServerStatus s : VirtualServerStatus.values()) {
@@ -84,6 +197,14 @@ public class ServerQueryInfo extends Wrapper {
 		return VirtualServerStatus.UNKNOWN;
 	}
 
+	/**
+	 * Gets the unique identifier of the currently selected virtual server.
+	 * <p>
+	 * If no virtual server has been selected yet, this method will return an empty string.
+	 * </p>
+	 *
+	 * @return the unique ID of the current virtual server
+	 */
 	public String getVirtualServerUniqueIdentifier() {
 		return get(VirtualServerProperty.VIRTUALSERVER_UNIQUE_IDENTIFIER);
 	}
