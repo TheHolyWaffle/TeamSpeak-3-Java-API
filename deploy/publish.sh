a380900a2533b4ff01f3aaa946c105a1cab6852a
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2017-04-20 01:00:27 +0200

    Move building additional JAR files into its own profile, 'full'

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    mvn deploy --settings $GPG_DIR/settings.xml -Pfull -DperformRelease=true -DskipTests=true
     exit $?
 fi
