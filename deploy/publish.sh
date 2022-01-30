d290c51233fd1532e4418530ea2b6785795009b7
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2022-01-30 19:33:35 +0100

    Remove old Travis-CI deploy scripts

--- deploy/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -Pfull -DperformRelease=true -DskipTests=true
-    exit $?
-fi
